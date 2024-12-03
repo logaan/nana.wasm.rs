60ee0b5d02a2a88167b8fc216adbd0d3aa9f53a7
Logan Campbell <colin@logaan.net>
2024-12-04 01:53:42 +1100

    Can parse titlecase

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,42 @@ mod bindings;
 
 use bindings::exports::wasi::cli::run::Guest as Command;
 
+use nom::{
+    character::complete::{alpha0, satisfy},
+    combinator::map,
+    sequence::tuple,
+    IResult,
+};
+
+fn uppercase_char(input: &str) -> IResult<&str, char> {
+    satisfy(|c| c.is_uppercase())(input)
+}
+
+fn titlecase_word(input: &str) -> IResult<&str, String> {
+    map(tuple((uppercase_char, alpha0)), |(first, rest)| {
+        format!("{}{}", first, rest)
+    })(input)
+}
+
+fn parse_uppercase() {
+    assert_eq!(uppercase_char("Foo"), Ok(("oo", 'F')));
+    assert_eq!(titlecase_word("Foo"), Ok(("", "Foo".to_string())));
+    assert_eq!(titlecase_word("Foo Bar"), Ok((" Bar", "Foo".to_string())));
+    assert_eq!(
+        titlecase_word("foo Bar"),
+        Err(nom::Err::Error(nom::error::Error {
+            input: "foo Bar",
+            code: nom::error::ErrorKind::Satisfy
+        }))
+    );
+}
+
 struct Component;
 
 impl Command for Component {
-    /// Say hello!
     fn run() -> Result<(), ()> {
-        println!("Hello world");
+        parse_uppercase();
+        println!("Tests passed");
         Ok(())
     }
 }
